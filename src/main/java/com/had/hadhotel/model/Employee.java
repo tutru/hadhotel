730937ec985cbd1bd13e9a79f4d0eef0a2c56a3c
Admin <Admin@DESKTOP-LQ0OBSB>
2019-09-21 22:17:22 +0700

    update Employee_model

--- src/main/java/com/had/hadhotel/model/Employee.java
+++ src/main/java/com/had/hadhotel/model/Employee.java
@@ -1,6 +1,129 @@
 package com.had.hadhotel.model;
 
+import java.util.Date;
+
 public class Employee {
-	private int id;
+	private int employeeid;
+	private String name;
+	private Date birthday;
+	private int governmentid;
+	private String phone;
+	private String email;
+	private String address;
+	private String image;
+	private Double salary;
+	
+	
+	public Employee() {
+		super();
+		
+	}
+
+
+	public Employee(int employeeid, String name, Date birthday, int governmentid, String phone, String email,
+			String address, String image, Double salary) {
+		super();
+		this.employeeid = employeeid;
+		this.name = name;
+		this.birthday = birthday;
+		this.governmentid = governmentid;
+		this.phone = phone;
+		this.email = email;
+		this.address = address;
+		this.image = image;
+		this.salary = salary;
+	}
+
+
+	public int getEmployeeid() {
+		return employeeid;
+	}
+
+
+	public void setEmployeeid(int employeeid) {
+		this.employeeid = employeeid;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+
+	public int getGovernmentid() {
+		return governmentid;
+	}
+
+
+	public void setGovernmentid(int governmentid) {
+		this.governmentid = governmentid;
+	}
+
+
+	public String getPhone() {
+		return phone;
+	}
+
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public String getImage() {
+		return image;
+	}
+
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+
+	public Double getSalary() {
+		return salary;
+	}
+
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	
+	
 
 }
