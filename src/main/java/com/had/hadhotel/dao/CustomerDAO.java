aabd2acd1a6962bb77a37af7d68d0a76b1bc87f9
Hao Duong <haodq4@gmail.com>
2019-09-25 20:33:14 +0700

    dsdsd

--- src/main/java/com/had/hadhotel/dao/CustomerDAO.java
+++ src/main/java/com/had/hadhotel/dao/CustomerDAO.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import com.had.hadhotel.entity.Customer;
 import com.had.hadhotel.model.CustomerInfo;
-
+//hao
 public interface CustomerDAO {
 	
 	public Customer findCustomer(int id);
