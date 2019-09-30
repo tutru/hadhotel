14d89745238ee42b0e50712228a198e09741204d
changeanh <trantheanh459@gmail.com>
2019-09-30 14:32:57 +0700

    xong

---
+++ src/main/java/com/had/hadhotel/controller/RoleComtroller.java
@@ -0,0 +1,28 @@
+package com.had.hadhotel.controller;
+
+import java.util.List;
+
+import javax.management.relation.RoleList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.had.hadhotel.dao.RoleDAO;
+import com.had.hadhotel.model.CustomerInfo;
+import com.had.hadhotel.model.RoleInfo;
+
+@Controller
+@RequestMapping("/")
+public class RoleComtroller {
+	@Autowired
+	private RoleDAO role;
+	
+	@RequestMapping("/rolelist")
+	public String roleList(Model model) {
+		List<RoleInfo> list = role.listRoleInfos();
+		model.addAttribute("list", list);
+		return "rolelist";
+	}
+}
